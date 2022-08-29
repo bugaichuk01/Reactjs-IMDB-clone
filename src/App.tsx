53e68a0714905419cc430045369d83d40c12d91d
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-29 23:24:56 +0300

    Trash commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
+import './styles/index.scss';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewFilms from "./components/NewFilms/NewFilms";
+
+
 
 function App() {
+/*  useEffect(() => {
+    axios.get(`https://api.kinopoisk.dev/movie?field=rating.kp&search=7-10&field=year&search=2017-2020&field=typeNumber&search=2&sortField=year&sortType=1&sortField=votes.imdb&sortType=-1&token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06`, {
+      headers: {
+        'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    }).then((r) => console.log(r))
+  }, [])*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewFilms />
     </div>
   );
 }
