08180497899db8acaf7930a92385cf36acf26201
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-31 21:34:15 +0300

    Add search list component

---
+++ src/components/Header/components/SearchList/SearchList.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import {useTypedSelector} from "../../../../_hooks/useTypedSelector";
+import {useGetFilmsBySearchQuery} from "../../../../_services/serviceAPI";
+import styles from './SearchList.module.scss';
+import {IMovie} from "../../../../_types/IMovie";
+import {ListItem} from "../ListItem/ListItem";
+
+interface SearchListProps {
+    debouncedValue: string
+}
+
+export const SearchList: React.FC<SearchListProps> = ({debouncedValue}) => {
+    const {currentOption} = useTypedSelector(state => state.dropDownReducer);
+    const {data, isFetching, refetch} = useGetFilmsBySearchQuery({type: currentOption.value, keyword: debouncedValue});
+
+    useEffect(() => {
+        refetch();
+    }, [debouncedValue])
+
+    return (
+        <div className={styles.search_list_container}>
+            <div className={styles.search_list}>
+                {data?.items?.length ? (
+                    <>
+                        {!isFetching ? (
+                            <>
+                                {data && data.items.slice(0, 8).map((item: IMovie) => (
+                                    <ListItem key={item.imdbId} item={item}/>
+                                ))}
+                            </>
+                        ) : (
+                            <div className={styles.loader}>
+                                loading
+                                {/*<Spinner variant='dark' size={SpinnerSizes.medium}/>*/}
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <p className={styles.no_results}>По вашему запросу ничего не найдено</p>
+                )}
+            </div>
+        </div>
+    );
+}
