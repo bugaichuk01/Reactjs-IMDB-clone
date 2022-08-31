3bd8fb4506b5439c513301fa50932197421ceacc
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-31 21:28:03 +0300

    Add dropDownReducer and searchReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,12 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {serviceAPI} from "../_services/serviceAPI";
+import {dropDownReducer} from "./reducers/dropDown.slice";
+import {searchReducer} from "./reducers/search.slice";
 
 const rootReducer = combineReducers({
     [serviceAPI.reducerPath]: serviceAPI.reducer,
+    dropDownReducer: dropDownReducer,
+    searchReducer: searchReducer
 })
 
 export const setupStore = () => {
