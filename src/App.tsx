18e81d99fe78b46b2d10e67d87e4869de5ebc020
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-31 21:56:11 +0300

    Trash commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import './styles/index.scss';
 import React from 'react';
 import './App.css';
 import NewFilms from "./components/NewFilms/NewFilms";
-
+import {Layout} from "./components/Layout/Layout";
+import {Header} from "./components/Header/Header";
 
 
 function App() {
-/*  useEffect(() => {
-    axios.get(`https://api.kinopoisk.dev/movie?field=rating.kp&search=7-10&field=year&search=2017-2020&field=typeNumber&search=2&sortField=year&sortType=1&sortField=votes.imdb&sortType=-1&token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06`, {
-      headers: {
-        'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    }).then((r) => console.log(r))
-  }, [])*/
+    /*  useEffect(() => {
+        axios.get(`https://api.kinopoisk.dev/movie?field=rating.kp&search=7-10&field=year&search=2017-2020&field=typeNumber&search=2&sortField=year&sortType=1&sortField=votes.imdb&sortType=-1&token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06`, {
+          headers: {
+            'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
+            'Content-Type': 'application/json'
+          }
+        }).then((r) => console.log(r))
+      }, [])*/
 
-  return (
-    <div className="App">
-      <NewFilms />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+     {/*       <Layout>
+                <NewFilms/>
+            </Layout>*/}
+        </div>
+    );
 }
 
 export default App;
