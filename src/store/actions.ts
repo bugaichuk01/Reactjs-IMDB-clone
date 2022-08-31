61a8320f6ca211051dd2bf3770e6f8a20214e074
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-31 21:15:51 +0300

    Add actions export

---
+++ src/store/actions.ts
@@ -0,0 +1,9 @@
+import {changeCurrentOption, toggleMenu} from "./reducers/dropDown.slice";
+import {setSearch, toggleSearch} from "./reducers/search.slice";
+
+export {
+    changeCurrentOption,
+    toggleMenu,
+    setSearch,
+    toggleSearch
+}
