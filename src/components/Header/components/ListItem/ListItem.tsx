e6480c8de89da516f8749ede5d08665e3b68556f
bugaichuk01 <bugaichuk001@gmail.com>
2022-08-31 21:21:09 +0300

    Add styles for list item component

---
+++ src/components/Header/components/ListItem/ListItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from "./ListItem.module.scss";
+import {capitalize} from "../../../../_helpers/capitalize";
+import {IMovie} from "../../../../_types/IMovie";
+
+interface ListIteProps {
+    item: IMovie
+}
+
+export const ListItem: React.FC<ListIteProps> = ({item}) => {
+    return (
+        <div className={styles.list_container}>
+            <div className={styles.image_container}>
+                <img src={item.posterUrl} />
+            </div>
+            <div className={styles.description}>
+                <span className={styles.title}>{item.nameRu}</span>
+                <span className={styles.info}>{item.year}</span>
+                <div>
+                    {item.genres.map((genre, index, genres) => (
+                        <span className={styles.info}>
+                            {capitalize(genre.genre)}{index !== (genres.length - 1) ? ', ' : ''}
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
