28cd7c74f3e9b5d6c55021a52e6653d43cd5bc4b
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-08 19:56:05 +0300

    Moved and some fixes added

--- src/components/Recommendations/NewFilms.tsx
+++ src/components/Recommendations/NewFilms.tsx
@@ -1,23 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useGetNewFilmsQuery} from "../../_services/serviceAPI";
-import {IMovie} from "../../types/IMovie";
 import styles from './NewFilms.module.scss'
 import cn from 'classnames';
 import {FilmItem} from "../FilmItem/FilmItem";
 
 export const NewFilms = () => {
     const {data} = useGetNewFilmsQuery();
-    const [newFilms, setNewFilms] = useState<IMovie[]>([]);
-
-    useEffect(() => {
-        data?.items && setNewFilms(data.items.slice(0, 10));
-    }, [data])
 
     return (
         <>
             <div className={cn('container', styles.container)}>
                 <ul className={cn("list", styles.grid)}>
-                    {newFilms?.map((item) => (
+                    {data?.items.slice(0, 10).map((item) => (
                         <FilmItem key={item.imdbId} item={item}/>
                     ))}
                 </ul>
@@ -26,4 +20,3 @@ export const NewFilms = () => {
     );
 }
 
-export default NewFilms;
