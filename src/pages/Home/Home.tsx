2ff7febb0dc4a28ae5893645d2bf08dc790da62f
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-08 19:55:42 +0300

    Add home page with test recommendations

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Layout} from "../../components/Layout/Layout";
+import {NewFilms} from "../../components/Recommendations/NewFilms";
+import {TopAwaitFilms} from "../../components/Recommendations/TopAwaitFilms";
 
 export const Home = () => {
     return (
-        <>
-
-        </>
+        <Layout>
+            <NewFilms />
+            <TopAwaitFilms />
+        </Layout>
     );
 }
 
