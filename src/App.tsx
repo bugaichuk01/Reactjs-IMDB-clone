3c192c60288810db7331afde23d102f71f618e2c
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-08 19:55:24 +0300

    Trash commit (only for test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import './styles/index.scss';
 import React from 'react';
 import './App.css';
-import NewFilms from "./components/NewFilms/NewFilms";
-import {Layout} from "./components/Layout/Layout";
-import {Header} from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 
 function App() {
-    /*  useEffect(() => {
-        axios.get(`https://api.kinopoisk.dev/movie?field=rating.kp&search=7-10&field=year&search=2017-2020&field=typeNumber&search=2&sortField=year&sortType=1&sortField=votes.imdb&sortType=-1&token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06`, {
-          headers: {
-            'X-API-KEY': `${process.env.REACT_APP_API_KEY}`,
-            'Content-Type': 'application/json'
-          }
-        }).then((r) => console.log(r))
-      }, [])*/
-
     return (
         <div className="App">
-            <Header />
-     {/*       <Layout>
-                <NewFilms/>
-            </Layout>*/}
+            <Home />
         </div>
     );
 }
