8e9b15ca5945c2c590dd23105f5c0c30d41e421e
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-09 03:08:00 +0300

    From 2 recommendations components to 1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Layout} from "../../components/Layout/Layout";
-import {NewFilms} from "../../components/Recommendations/NewFilms";
-import {NewSeries} from "../../components/Recommendations/NewSeries";
+import {useGetNewFilmsQuery, useGetNewSeriesQuery} from "../../_services/serviceAPI";
+import {Recommendations} from "../../components/Recommendations/Recommendations";
 
 export const Home = () => {
+    const films = useGetNewFilmsQuery();
+    const series = useGetNewSeriesQuery();
+
+
     return (
         <Layout>
-            <NewFilms />
-            <NewSeries />
+            <Recommendations data={films.data} title='New films' />
+            <Recommendations data={series.data} title='New series' />
         </Layout>
     );
 }
