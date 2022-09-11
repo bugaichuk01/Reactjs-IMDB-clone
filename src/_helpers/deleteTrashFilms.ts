75e815531e2602eed6f2f0dd27863326327f367a
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-11 22:15:02 +0300

    Remove slice method

--- src/_helpers/deleteTrashFilms.ts
+++ src/_helpers/deleteTrashFilms.ts
@@ -1,5 +1,5 @@
 import {IMovie} from "../types/IMovie";
 
 export const deleteTrashFilms = (data: IMovie[] | undefined) => {
-    return data && data.filter(item => item.nameOriginal != null).slice(0, 10)
+    return data && data.filter(item => item.nameOriginal != null)
 }
