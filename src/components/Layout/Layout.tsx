c87c4bf222e1950faf23ae2b0aeb8dc20da4f560
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-11 03:50:30 +0300

    Add React import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import {FC, PropsWithChildren} from "react"
+import React, {FC, PropsWithChildren} from "react"
 import {Header} from "../Header/Header";
 
 export const Layout: FC<PropsWithChildren<{}>> = ({children}) => {
