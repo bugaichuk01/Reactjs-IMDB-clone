70415af919d5512af98cfa5ab000cbc76aa44f86
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-11 03:49:16 +0300

    Move slider to own folder

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import styles from './Recommendations.module.scss';
 import cn from 'classnames';
-import {FilmItem} from "../FilmItem/FilmItem";
 import {UiButton} from "../UI/UIButton/UIButton";
 import {IMovies} from "../../types/IMovie";
-//@ts-ignore
-import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import {FaChevronRight} from "react-icons/fa";
-import {FaChevronLeft} from "react-icons/fa";
+import {RecommendationsSlider} from "../RecommendationsSlider/RecommendationsSlider";
 import {deleteTrashFilms} from "../../_helpers/deleteTrashFilms";
 
 interface RecommendationsTypes {
@@ -16,29 +12,7 @@ interface RecommendationsTypes {
     title: string
 }
 
-interface renderButtonTypes {
-    isDisabled: boolean
-}
-
-const responsive = {
-    0: {items: 1},
-    568: {items: 2},
-    1024: {items: 6},
-};
-
 export const Recommendations: React.FC<RecommendationsTypes> = ({data, title}) => {
-
-    const renderPrevButton = ({isDisabled}: renderButtonTypes) => {
-        return <div className={isDisabled ? styles.disabled : cn(styles.thumb, styles.thumb_left)}>
-            <FaChevronLeft/>
-        </div>
-    };
-
-    const renderNextButton = ({isDisabled}: renderButtonTypes) => {
-        return <div className={isDisabled ? styles.disabled : cn(styles.thumb, styles.thumb_right)}>
-            <FaChevronRight/>
-        </div>
-    };
 
     return (
         <div className={cn('container', styles.container)}>
@@ -47,17 +21,7 @@ export const Recommendations: React.FC<RecommendationsTypes> = ({data, title}) =
                 <UiButton style={styles.button} text='Смотреть все'/>
             </div>
 
-            <AliceCarousel
-                mouseTracking
-                items={deleteTrashFilms(data?.items).map((item) => (
-                    <FilmItem key={item.imdbId} item={item}/>
-                ))}
-                responsive={responsive}
-                controlsStrategy="alternate"
-                disableDotsControls={true}
-                renderPrevButton={renderPrevButton}
-                renderNextButton={renderNextButton}
-            />
+            <RecommendationsSlider data={deleteTrashFilms(data?.items)}/>
         </div>
     );
 }
---
+++ src/components/RecommendationsSlider/RecommendationsSlider.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from "../Recommendations/Recommendations.module.scss";
+import cn from "classnames";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
+import AliceCarousel from "react-alice-carousel";
+import {IMovie} from "../../types/IMovie";
+import {FilmItem} from "../FilmItem/FilmItem";
+
+interface renderButtonTypes {
+    isDisabled: boolean
+}
+
+interface RecommendationsSliderTypes {
+    data: IMovie[] | undefined;
+}
+
+const responsive = {
+    0: {items: 1},
+    568: {items: 2},
+    1024: {items: 6},
+};
+
+export const RecommendationsSlider: React.FC<RecommendationsSliderTypes> = ({data}) => {
+    const renderPrevButton = ({isDisabled}: renderButtonTypes) => {
+        return <div className={isDisabled ? styles.disabled : cn(styles.thumb, styles.thumb_left)}>
+            <FaChevronLeft/>
+        </div>
+    };
+
+    const renderNextButton = ({isDisabled}: renderButtonTypes) => {
+        return <div className={isDisabled ? styles.disabled : cn(styles.thumb, styles.thumb_right)}>
+            <FaChevronRight/>
+        </div>
+    };
+
+    return (
+        <AliceCarousel
+            mouseTracking
+            items={data?.map((item) => (
+                <FilmItem key={item.imdbId} item={item}/>
+            ))}
+            responsive={responsive}
+            controlsStrategy="alternate"
+            disableDotsControls={true}
+            renderPrevButton={renderPrevButton}
+            renderNextButton={renderNextButton}
+        />
+    );
+}
+
