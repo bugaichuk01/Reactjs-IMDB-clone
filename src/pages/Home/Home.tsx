d7f59752bff3992cfd5caf2f85845533a3296f47
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-12 02:37:46 +0300

    Test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import {Layout} from "../../components/Layout/Layout";
-import {useGetNewFilmsQuery, useGetNewSeriesQuery} from "../../_services/serviceAPI";
-import {Recommendations} from "../../components/Recommendations/Recommendations";
+import {useGetNewFilmsQuery, useGetNewSeriesQuery, useGetPremiersQuery} from "../../_services/serviceAPI";
+import {Popular} from "../../components/Popular/Popular";
+import {FilmsGroup} from "../../components/FilmsGroup/FilmsGroup";
 
 export const Home = () => {
     const films = useGetNewFilmsQuery();
     const series = useGetNewSeriesQuery();
+    const premiers = useGetPremiersQuery();
 
     return (
         <Layout>
-            <Recommendations data={films.data} title={'Популярные фильмы текущего года'} />
-            <Recommendations data={series.data} title={'Популярные сериалы текущего года'} />
+            {/*
+            <Popular data={films.data} title={'Популярные фильмы текущего года'}/>
+            <Popular data={series.data} title={'Популярные сериалы текущего года'}/>
+            */}
+            <FilmsGroup data={premiers.data} title='Цифровые релизы этого месяца'/>
         </Layout>
     );
 }
