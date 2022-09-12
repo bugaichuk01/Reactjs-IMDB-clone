cfd6deefa441501c68612f9c951ba9ee6b929e63
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 00:10:11 +0300

    Add watchlist buttons

--- src/components/FilmItem/FilmItem.tsx
+++ src/components/FilmItem/FilmItem.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {IMovie} from "../../types/IMovie";
 import styles from './FilmItem.module.scss'
 import {FaStar} from "react-icons/fa";
+import {UIWatchListButton} from "../UI/UIWatchListButton/UIWatchListButton";
+import {UIFlag} from "../UI/UIFlag/UIFlag";
 
 interface FilmItemProps {
     item: IMovie
@@ -11,12 +13,14 @@ export const FilmItem: React.FC<FilmItemProps> = ({item}) => {
     return (
         <li className={styles.list_item}>
             <img className={styles.image} src={item.posterUrl} alt=""/>
+            <UIFlag />
             <div className={styles.bottom}>
                 <div className={styles.rating_group}>
                     <FaStar className={styles.icon}/>
                     <span className={styles.rating}>{item.ratingKinopoisk}</span>
                 </div>
                 <span className={styles.name}>{item.nameRu}</span>
+                <UIWatchListButton />
             </div>
         </li>
     );
