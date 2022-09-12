316883649dfc9dbec6a79188c7efb3b2c7454474
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 00:13:46 +0300

    Little changes

--- src/components/Popular/Popular.tsx
+++ src/components/Popular/Popular.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Popular.module.scss';
-import cn from 'classnames';
 import {UiButton} from "../UI/UIButton/UIButton";
 import {IMovies} from "../../types/IMovie";
 import {Slider} from "../Slider/Slider";
@@ -15,13 +14,14 @@ interface PopularTypes {
 export const Popular: React.FC<PopularTypes> = ({data, title}) => {
 
     return (
-        <div className={cn('container', styles.container)}>
+        <div className='container'>
             <div className={styles.top}>
                 <h1>{title}</h1>
                 <UiButton style={styles.button} text='Смотреть все'/>
             </div>
 
             <Slider
+                itemsNumber={6}
                 data=
                     {deleteTrashFilms(data?.items)?.map((item) => (
                         <FilmItem key={item.imdbId} item={item}/>
