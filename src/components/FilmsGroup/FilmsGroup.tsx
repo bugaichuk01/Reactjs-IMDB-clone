69c7790a73a755e7b77b61fc72ca8cd99f4ab8ee
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 00:18:17 +0300

    Fix FilmsGroup component

--- src/components/FilmsGroup/FilmsGroup.tsx
+++ src/components/FilmsGroup/FilmsGroup.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import {IMovies} from "../../types/IMovie";
+import {IMovie} from "../../types/IMovie";
+import styles from './FilmsGroup.module.scss';
 
 interface FilmsGroupTypes {
-    data: IMovies | undefined;
+    data: IMovie[] | undefined;
     title: string;
 }
 
 export const FilmsGroup: React.FC<FilmsGroupTypes> = ({data, title}) => {
     return (
-        <div className={'container'} style={{width: '100%'}}>
-            <div style={{width: '400px', height: '220px', display: 'flex', flexDirection: 'column'}}>
-                <div style={{width: '100%', height: '100%',display: 'flex', flexDirection: 'row'}}>
-                    {data?.items.slice(0, 3).map((item) => (
-                        <img style={{width: '100%', height: '100%'}} src={item.posterUrl} alt=""/>
-                    ))}
-                </div>
-                <span style={{color: 'white', fontSize: '1rem', alignSelf: 'start'}}>{title}</span>
+        <div className={styles.wrapper}>
+            <div className={styles.films}>
+                {data?.slice(0, 3).map((item, index) => (
+                    <img key={index} src={item.posterUrl} alt={item.nameRu}/>
+                ))}
             </div>
+            <span className={styles.description}>{title}</span>
         </div>
     );
 }
