49442bd06d7fbe5693990aa1ad5893acc608ebff
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 03:01:49 +0300

    Little changes

--- src/components/FilmItem/FilmItem.tsx
+++ src/components/FilmItem/FilmItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {IMovie} from "../../types/IMovie";
 import styles from './FilmItem.module.scss'
+import {IMovie} from "../../types/IMovie";
 import {FaStar} from "react-icons/fa";
-import {UIWatchListButton} from "../UI/UIWatchListButton/UIWatchListButton";
-import {UIFlag} from "../UI/UIFlag/UIFlag";
+import {Bookmark} from "../UI/Bookmark/Bookmark";
+import {WatchlistButton} from "../UI/WatchListButton/WatchlistButton";
 
 interface FilmItemProps {
     item: IMovie
@@ -13,14 +13,14 @@ export const FilmItem: React.FC<FilmItemProps> = ({item}) => {
     return (
         <li className={styles.list_item}>
             <img className={styles.image} src={item.posterUrl} alt=""/>
-            <UIFlag />
+            <Bookmark />
             <div className={styles.bottom}>
                 <div className={styles.rating_group}>
                     <FaStar className={styles.icon}/>
                     <span className={styles.rating}>{item.ratingKinopoisk}</span>
                 </div>
                 <span className={styles.name}>{item.nameRu}</span>
-                <UIWatchListButton />
+                <WatchlistButton />
             </div>
         </li>
     );
