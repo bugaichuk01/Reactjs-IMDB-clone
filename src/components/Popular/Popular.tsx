607b0670d93b52d17250b95f7131427a34de1478
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 03:03:02 +0300

    Little changes

--- src/components/Popular/Popular.tsx
+++ src/components/Popular/Popular.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Popular.module.scss';
-import {UiButton} from "../UI/UIButton/UIButton";
 import {IMovies} from "../../types/IMovie";
+import {Button} from "../UI/Button/Button";
 import {Slider} from "../Slider/Slider";
-import {deleteTrashFilms} from "../../_helpers/deleteTrashFilms";
 import {FilmItem} from "../FilmItem/FilmItem";
+import {deleteTrashFilms} from "../../_helpers/deleteTrashFilms";
 
 interface PopularTypes {
     data: IMovies | undefined;
@@ -17,15 +17,14 @@ export const Popular: React.FC<PopularTypes> = ({data, title}) => {
         <div className='container'>
             <div className={styles.top}>
                 <h1>{title}</h1>
-                <UiButton style={styles.button} text='Смотреть все'/>
+                <Button style={styles.button} text='Смотреть все'/>
             </div>
 
             <Slider
                 itemsNumber={6}
-                data=
-                    {deleteTrashFilms(data?.items)?.map((item) => (
-                        <FilmItem key={item.imdbId} item={item}/>
-                    ))}
+                data={deleteTrashFilms(data?.items)?.map((item) => (
+                    <FilmItem key={item.imdbId} item={item}/>
+                ))}
             />
         </div>
     );
