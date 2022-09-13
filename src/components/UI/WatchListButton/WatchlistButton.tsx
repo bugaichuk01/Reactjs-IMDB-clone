14f1ea5da2591b766b9e5d115b374a5ab4e96551
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-13 03:02:05 +0300

    Change file name

--- src/components/UI/WatchListButton/WatchlistButton.tsx
+++ src/components/UI/WatchListButton/WatchlistButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from './UIWatchListButton.module.scss';
 import cn from 'classnames';
+import styles from './WatchlistButton.module.scss';
 import {BsPlusLg} from "react-icons/bs";
 
 interface UIButtonTypes {
     style?: string;
 }
 
-export const UIWatchListButton: React.FC<UIButtonTypes> = ({style}) => {
+export const WatchlistButton: React.FC<UIButtonTypes> = ({style}) => {
     return (
         <button className={cn(styles.button, style)}>
             <BsPlusLg className={styles.icon} />
