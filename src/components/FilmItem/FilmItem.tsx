c802769ceff41b1e2b8246a8e5855a9209200e84
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-14 19:39:47 +0300

    Added information open logic and trailer button

--- src/components/FilmItem/FilmItem.tsx
+++ src/components/FilmItem/FilmItem.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './FilmItem.module.scss'
 import {IMovie} from "../../types/IMovie";
-import {FaStar} from "react-icons/fa";
-import {Bookmark} from "../UI/Bookmark/Bookmark";
-import {WatchlistButton} from "../UI/WatchListButton/WatchlistButton";
+import {FaPlay, FaStar} from "react-icons/fa";
+import {Prompt} from "../Prompt/Prompt";
+import {Info} from "./components/Info/Info";
+import {Bookmark} from "./components/Bookmark/Bookmark";
+import {Button} from "../UI/Button/Button";
+import {BsPlusLg} from "react-icons/bs";
 
 interface FilmItemProps {
     item: IMovie
 }
 
 export const FilmItem: React.FC<FilmItemProps> = ({item}) => {
+    const [isInfoOpen, setIsInfoOpen] = useState<boolean>(false);
+
     return (
         <li className={styles.list_item}>
-            <img className={styles.image} src={item.posterUrl} alt=""/>
-            <Bookmark />
+            {isInfoOpen && <Prompt id={item.kinopoiskId.toString()} isOpen={isInfoOpen} onClose={setIsInfoOpen}/>}
+
+            <img className={styles.image} src={item.posterUrl} alt={item.nameOriginal}/>
+            <Bookmark/>
             <div className={styles.bottom}>
                 <div className={styles.rating_group}>
                     <FaStar className={styles.icon}/>
                     <span className={styles.rating}>{item.ratingKinopoisk}</span>
                 </div>
                 <span className={styles.name}>{item.nameRu}</span>
-                <WatchlistButton />
+                <Button style={styles.watchlist_btn}>
+                    <BsPlusLg/>
+                    Буду смотреть
+                </Button>
+                <div className={styles.film_info}>
+                    <Button style={styles.trailer_btn}>
+                        <FaPlay/>
+                        Трейлер
+                    </Button>
+                    <Info onClick={() => setIsInfoOpen(true)}/>
+                </div>
             </div>
         </li>
     );
