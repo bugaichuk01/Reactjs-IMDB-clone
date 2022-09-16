a15d7883d9b5ccf907ba7ba4b22c775d49f23b39
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-16 21:57:33 +0300

    Change logic

--- src/components/Prompt/Prompt.tsx
+++ src/components/Prompt/Prompt.tsx
@@ -9,6 +9,8 @@ import {useGetFilmByIdQuery} from "../../_services/serviceAPI";
 import {useOnClickOutside} from "usehooks-ts";
 import {Button} from "../UI/Button/Button";
 import {BsPlusLg} from "react-icons/bs";
+import {Content} from "../Content/Content";
+import {EnumInfo} from "../EnumInfo/EnumInfo";
 
 interface PromptTypes {
     id: string;
@@ -17,7 +19,7 @@ interface PromptTypes {
 }
 
 export const Prompt: React.FC<PromptTypes> = ({id, isOpen, onClose}) => {
-    const {data} = useGetFilmByIdQuery(id);
+    const {data, isFetching, isLoading} = useGetFilmByIdQuery(id);
 
     const divRef = useRef<HTMLDivElement>(null);
     useOnClickOutside(divRef, () => onClose(false));
@@ -27,44 +29,45 @@ export const Prompt: React.FC<PromptTypes> = ({id, isOpen, onClose}) => {
     return createPortal(
         <div className={styles.prompt}>
             <div ref={divRef} className={styles.prompt_dialog}>
-                <div className={styles.close_btn} onClick={() => onClose(false)}>
-                    <FaTimes/>
-                </div>
-                <div className={styles.prompt_panel}>
-                    <div className={styles.prompt_content}>
-                        <div className={styles.header}>
-                            <img className={styles.poster} src={data?.posterUrl} alt={data?.nameOriginal}/>
-                            <div className={styles.header_info}>
-                                <div className={styles.title}>
-                                    <h3>{data?.nameRu}</h3>
-                                </div>
-                                <ul className={styles.list}>
-                                    <li>{data?.year}</li>
-                                    <li className={styles.secondary_item}>{getTimeFromMinutes(data?.filmLength)}</li>
-                                    <li className={styles.secondary_item}>{convertAgeLimit(data?.ratingAgeLimits)}</li>
-                                </ul>
-                                <ul className={styles.list}>
-                                    {data?.genres.map((genre) => (
-                                        <li className={styles.secondary_genre}
-                                            key={genre.genre}>{capitalize(genre.genre)}</li>
-                                    ))}
-                                </ul>
-                                <div className={styles.rating_group}>
-                                    <FaStar className={styles.icon}/>
-                                    <div>
-                                        <span>{data?.ratingKinopoisk}</span>
-                                        <span className={styles.rating_secondary}>/10</span>
+                <Content data={data} isLoading={isLoading} isFetching={isFetching}>
+                    <div className={styles.close_btn} onClick={() => onClose(false)}>
+                        <FaTimes/>
+                    </div>
+                    <div className={styles.prompt_panel}>
+                        <div className={styles.prompt_content}>
+                            <div className={styles.header}>
+                                <img className={styles.poster} src={data?.posterUrl} alt={data?.nameOriginal}/>
+                                <div className={styles.header_info}>
+                                    <div className={styles.title}>
+                                        <h3>{data?.nameRu}</h3>
+                                    </div>
+                                    <EnumInfo>
+                                        <li>{data?.year}</li>
+                                        <li>{getTimeFromMinutes(data?.filmLength)}</li>
+                                        <li>{convertAgeLimit(data?.ratingAgeLimits)}</li>
+                                    </EnumInfo>
+                                    <EnumInfo>
+                                        {data?.genres.map((genre) => (
+                                            <li key={genre.genre}>{capitalize(genre.genre)}</li>
+                                        ))}
+                                    </EnumInfo>
+                                    <div className={styles.rating_group}>
+                                        <FaStar className={styles.icon}/>
+                                        <div>
+                                            <span>{data?.ratingKinopoisk}</span>
+                                            <span className={styles.rating_secondary}>/10</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div className={styles.description}>{data?.description}</div>
+                            <Button style={styles.watchlist_btn}>
+                                <BsPlusLg/>
+                                Буду смотреть
+                            </Button>
                         </div>
-                        <div className={styles.description}>{data?.description}</div>
-                        <Button style={styles.watchlist_btn}>
-                            <BsPlusLg/>
-                            Буду смотреть
-                        </Button>
                     </div>
-                </div>
+                </Content>
             </div>
         </div>,
         document.body
