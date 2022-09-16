096f711d2436464e26aa43add4bb29daff159da5
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-16 21:56:39 +0300

    Delete useless style

--- src/components/FilmsTitle/FilmsTitle.tsx
+++ src/components/FilmsTitle/FilmsTitle.tsx
@@ -9,7 +9,7 @@ interface FilmsTitleTypes {
 
 export const FilmsTitle: React.FC<FilmsTitleTypes> = ({title, description, info}) => {
     return (
-        <div className={styles.main}>
+        <div>
             <h3 className={styles.title}>{title}</h3>
             <h3 className={styles.editors_pick}>{description}</h3>
             <h3 className={styles.title_info }>{info}</h3>
