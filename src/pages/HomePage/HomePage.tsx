12cb8998cac694f57ea832ac38698ae27a00b8d8
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-16 21:56:09 +0300

    Change logic

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout} from "../../components/Layout/Layout";
+import {Home} from "../../components/screens/Home/Home";
+
+export const HomePage = () => {
+
+    return (
+        <Layout>
+            <Home />
+        </Layout>
+    );
+}
