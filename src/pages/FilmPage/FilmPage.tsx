1249b6488489f05a246d0577301c2af8881da13d
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-16 21:58:13 +0300

    Added film page

---
+++ src/pages/FilmPage/FilmPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout} from "../../components/Layout/Layout";
+import {Film} from "../../components/screens/Film/Film";
+
+export const FilmPage = () => {
+
+    return (
+        <Layout>
+            <Film />
+        </Layout>
+    );
+}
