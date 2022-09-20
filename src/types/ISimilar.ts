aa43624c92be09a089273a0cc4830bdb79c66d76
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 03:31:40 +0300

    Create new types file

---
+++ src/types/ISimilar.ts
@@ -0,0 +1,9 @@
+export interface ISimilar {
+    filmId: number,
+    nameRu?: string,
+    nameEn?: string,
+    nameOriginal?: string,
+    posterUrl?: string,
+    posterUrlPreview?: string,
+    relationType: string
+}
