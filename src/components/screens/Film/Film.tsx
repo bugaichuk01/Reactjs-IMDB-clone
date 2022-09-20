f07f7583627bb28ffaa376d364b3bf838ed57878
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 03:33:16 +0300

    Upgrade component

--- src/components/screens/Film/Film.tsx
+++ src/components/screens/Film/Film.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import {useGetFilmByIdQuery} from "../../../_services/serviceAPI";
-import {useParams} from "react-router";
 import styles from './Film.module.scss';
-import {convertMovieType} from "../../../_helpers/convertMovieType";
-import {convertAgeLimit} from "../../../_helpers/convertAgeLimit";
-import {Info} from "./components/Info/Info";
-import {getTimeFromMinutes} from "../../../_helpers/getTimeFromMinutes";
-import {EnumInfo} from "../../EnumInfo/EnumInfo";
-import {Bookmark} from "../../FilmItem/components/Bookmark/Bookmark";
+import {useParams} from "react-router";
 import {useGetBoxOffice} from "../../../_hooks/useGetBoxOffice";
+import {useGetFilmByIdQuery} from "../../../_services/serviceAPI";
 import {capitalize} from "../../../_helpers/capitalize";
+import {convertAgeLimit} from "../../../_helpers/convertAgeLimit";
+import {convertMovieType} from "../../../_helpers/convertMovieType";
+import {getTimeFromMinutes} from "../../../_helpers/getTimeFromMinutes";
+import {Info} from "./components/Info/Info";
+import {EnumInfo} from "../../EnumInfo/EnumInfo";
 import {Rating} from "./components/Rating/Rating";
+import {Bookmark} from "../../FilmItem/components/Bookmark/Bookmark";
+import {Tabs} from "../../Tabs/Tabs";
+import {Similar} from "./components/Similar/Similar";
+import {Actors} from "./components/Actors/Actors";
+import {Facts} from "./components/Facts/Facts";
 
 interface ElementTypes {
     value: string;
-    id: number;
+    id: number | undefined;
 }
 
 export const Element: React.FC<ElementTypes> = ({value, id}) => <React.Fragment>{id ? ', ' : ''}{value}</React.Fragment>
 
 export const Film = () => {
     const {id} = useParams();
-    const boxOffice = useGetBoxOffice(id);
-    const {data, isLoading} = useGetFilmByIdQuery(id as string);
+    const boxOffice = useGetBoxOffice(Number(id));
+    const {data, isLoading} = useGetFilmByIdQuery(Number(id));
 
     const {
         countries,
@@ -37,7 +41,8 @@ export const Film = () => {
         posterUrl,
         ratingKinopoisk,
         ratingImdb,
-        ratingGoodReview
+        ratingGoodReview,
+        description
     } = {...data}
 
     const items = [
@@ -54,38 +59,49 @@ export const Film = () => {
         {caption: 'Слоган', value: slogan, condition: slogan},
     ]
 
+    const tabs = [
+        {txt: 'Описание', content: <p className={styles.desc}>{description}</p>, condition: description?.length},
+        {txt: 'Актёры', content: <Actors filmId={data?.kinopoiskId}/>},
+        {txt: 'Факты', content: <Facts filmId={data?.kinopoiskId}/>}
+    ]
+
     const movieYear = year && `(${year})`;
     const movieTitle = nameRu ? nameRu : isLoading ? 'Загрузка' : 'Без названия';
-    const totalItems = boxOffice && items.concat(boxOffice);
+    const totalItems = boxOffice && [...items, ...boxOffice];
 
     return (
         <>
             <div className={styles.container}>
-                <h1 className={styles.title}>
-                    {movieTitle} {movieYear}
-                </h1>
-                <div className={styles.originalTitle}>
-                    <span>Original title: {nameOriginal}</span>
-                    <EnumInfo>
-                        <li>{year}</li>
-                        <li>{getTimeFromMinutes(filmLength)}</li>
-                        <li>{convertAgeLimit(ratingAgeLimits)}</li>
-                    </EnumInfo>
-                </div>
-                <div className={styles.content}>
-                    <div className={styles.left}>
-                        <Bookmark/>
-                        <img className={styles.image} src={posterUrl} alt={nameOriginal}/>
+                <div>
+                    <h1 className={styles.title}>
+                        {movieTitle} {movieYear}
+                    </h1>
+                    <div className={styles.originalTitle}>
+                        <span>Original title: {nameOriginal}</span>
+                        <EnumInfo>
+                            <li>{year}</li>
+                            <li>{getTimeFromMinutes(filmLength)}</li>
+                            <li>{convertAgeLimit(ratingAgeLimits)}</li>
+                        </EnumInfo>
                     </div>
+                    <div className={styles.content}>
+                        <div className={styles.left}>
+                            <Bookmark/>
+                            <img className={styles.image} src={posterUrl} alt={nameOriginal}/>
+                        </div>
 
-                    <div className={styles.right}>
-                        <h2 className={styles.subtitle}>
-                            О {convertMovieType(type)}е
-                        </h2>
-                        <Rating ratingKinopoisk={ratingKinopoisk} ratingImdb={ratingImdb} ratingGoodReview={ratingGoodReview} />
-                        <Info items={totalItems}/>
+                        <div className={styles.right}>
+                            <h2 className={styles.subtitle}>
+                                О {convertMovieType(type)}е
+                            </h2>
+                            <Rating ratingKinopoisk={ratingKinopoisk} ratingImdb={ratingImdb}
+                                    ratingGoodReview={ratingGoodReview}/>
+                            <Info items={totalItems}/>
+                        </div>
                     </div>
                 </div>
+                <Tabs tabs={tabs}/>
+                <Similar id={data?.kinopoiskId} />
             </div>
         </>
     );
