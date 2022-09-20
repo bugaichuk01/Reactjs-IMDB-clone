05ad53e8ad59ccdfe37c8732abc1610dbc263ba3
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 03:32:45 +0300

    Change logic

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,10 +2,13 @@ import React, {PropsWithChildren} from 'react';
 import styles from './Content.module.scss';
 import ReactLoading from "react-loading";
 import {IMovie} from "../../types/IMovie";
+import {IBaseItems} from "../../types/IQuery";
+import {ISimilar} from "../../types/ISimilar";
 
-interface ContentTypes {
-    data?: IMovie;
-    items?: IMovie[];
+type Items = ISimilar | IMovie;
+
+interface ContentTypes<T> {
+    data: T | undefined;
     children: React.ReactNode;
     isLoading: boolean;
     isFetching: boolean;
@@ -21,13 +24,12 @@ export const Loader = () => {
 
 export const Error = ({children}: PropsWithChildren<{}>) => <p className={styles.no_results}>{children}</p>
 
-export const Content: React.FC<ContentTypes> = ({data, items, children, isLoading, isFetching}) => {
-
+export const Content: React.FC<ContentTypes<IBaseItems<Items>>> = ({data, children, isLoading, isFetching}) => {
     return (
         <>
             {isLoading || isFetching ? <Loader/> : (
                 <div className={styles.content}>
-                    {!data && !items?.length ? <Error>Ничего не найдено!</Error> : children}
+                    {!data?.items.length ? <Error>Ничего не найдено!</Error> : children}
                 </div>
             )}
         </>
