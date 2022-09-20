f430b0af998a7e74c2157c8eead7609b93521e9a
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 03:29:46 +0300

    Change logic

--- src/_hooks/useGetBoxOffice.ts
+++ src/_hooks/useGetBoxOffice.ts
@@ -1,16 +1,12 @@
 import {useGetBoxOfficeQuery} from "../_services/serviceAPI";
 import {convertBoxOffice} from "../_helpers/convertBoxOffice";
 import {converPrice} from "../_helpers/convertPrice";
+import {IBoxOffice} from "../types/IBoxOffice";
 
-type boxOfficeTypes = {
-    type: string;
-    amount: number;
-}
+export const useGetBoxOffice = (id: number | undefined) => {
+    const {data} = useGetBoxOfficeQuery(id);
 
-export const useGetBoxOffice = (id: string | undefined) => {
-    const boxOffice = useGetBoxOfficeQuery(id as string);
-
-    return boxOffice?.data?.items.map((item: boxOfficeTypes) => (
+    return data?.items.map((item: IBoxOffice) => (
         {caption: `${convertBoxOffice(item.type)}`, value: `${item.type !== 'BUDGET' ? '+' : ''} ${converPrice(item.amount)} $`, condition: item.amount}
     ));
 }
