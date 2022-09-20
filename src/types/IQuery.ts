eb8ac0edda263b426ed451af7140a6452acccaf6
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 03:33:48 +0300

    Added new interface

--- src/types/IQuery.ts
+++ src/types/IQuery.ts
@@ -2,3 +2,10 @@ export interface ISearchQuery {
     type: string;
     keyword: string;
 }
+
+export interface IBaseItems<T> {
+    items: T[];
+    films?: T[];
+    total: number;
+    totalPage?: number;
+}
