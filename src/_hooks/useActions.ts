0953e9a4ece510565ede560e97a64ef8e4a4431a
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-04 22:37:26 +0300

    Removed an empty line

--- src/_hooks/useActions.ts
+++ src/_hooks/useActions.ts
@@ -2,7 +2,6 @@ import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as AllActions from '../store/actions'
 
-
 export const useActions = () => {
     const dispatch = useDispatch()
 
