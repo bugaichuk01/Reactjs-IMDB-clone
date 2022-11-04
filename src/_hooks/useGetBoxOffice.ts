502498ce153c92c815f770ed031c7a5cf0804b83
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-04 22:47:32 +0300

    Refactor

--- src/_hooks/useGetBoxOffice.ts
+++ src/_hooks/useGetBoxOffice.ts
@@ -1,12 +1,11 @@
-import {useGetBoxOfficeQuery} from "../_services/serviceAPI";
-import {convertBoxOffice} from "../_helpers/convertBoxOffice";
-import {converPrice} from "../_helpers/convertPrice";
 import {IBoxOffice} from "../types/IBoxOffice";
+import converter from '_/converters';
+import {useGetBoxOfficeQuery} from "_/serviceAPI";
 
 export const useGetBoxOffice = (id: number | undefined) => {
     const {data} = useGetBoxOfficeQuery(id);
 
     return data?.items.map((item: IBoxOffice) => (
-        {caption: `${convertBoxOffice(item.type)}`, value: `${item.type !== 'BUDGET' ? '+' : ''} ${converPrice(item.amount)} $`, condition: item.amount}
+        {caption: `${converter.convertBoxOffice(item.type)}`, value: `${item.type !== 'BUDGET' ? '+' : ''} ${converter.convertPrice(item.amount)} $`, condition: item.amount}
     ));
 }
