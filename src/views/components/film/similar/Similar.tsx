21c91027d3644c29e91112ae1b8da465bebd26a0
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-05 19:10:03 +0300

    Change id logic

--- src/views/components/film/similar/Similar.tsx
+++ src/views/components/film/similar/Similar.tsx
@@ -4,25 +4,21 @@ import {SimilarItem} from "../similar-item/SimilarItem";
 import {Slider} from "@/shared-components/slider/Slider";
 import {Caption} from "@/shared-components/caption/Caption";
 import {Content} from "@/layout-components/content/Content";
-import { useParams } from "react-router-dom";
 
-export const Similar = () => {
-    const {id} = useParams();
-    const {data, isLoading, isFetching} = useGetSimilarQuery(Number(id))
+interface SimilarProps {
+    id: number | undefined
+}
+
+export const Similar: React.FC<SimilarProps> = ({id}) => {
+    const {data, isLoading, isFetching} = useGetSimilarQuery(id)
 
     return (
         <>
             {data && data.total < 4
-                ? (
-                    <div>
-                        <Caption description={`К сожалению, мы не смогли найти похожих фильмов :(`}/>
-                    </div>
-                )
+                ? <Caption description={`К сожалению, мы не смогли найти похожих фильмов :(`}/>
                 : (
                     <>
-                        <div>
-                            <Caption description={`Вам также может понравиться`} title='Похожее кино'/>
-                        </div>
+                        <Caption description={`Вам также может понравиться`} title='Похожее кино'/>
 
                         <Content data={data} isLoading={isLoading} isFetching={isFetching}>
                             <Slider
