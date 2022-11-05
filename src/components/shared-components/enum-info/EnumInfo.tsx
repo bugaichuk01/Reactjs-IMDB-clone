bce824bfb51145aae93a7bbeb2cd6c13ffd6baa3
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-05 20:41:43 +0300

    Deleted list style

--- src/components/shared-components/enum-info/EnumInfo.tsx
+++ src/components/shared-components/enum-info/EnumInfo.tsx
@@ -1,10 +1,9 @@
 import React, {PropsWithChildren} from 'react';
 import styles from "./EnumInfo.module.scss";
-import cn from 'classnames';
 
 export const EnumInfo = ({children}: PropsWithChildren<{}>) => {
     return (
-        <ul className={cn('list', styles.list)}>
+        <ul className={styles.list}>
             {children}
         </ul>
     );
