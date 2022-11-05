cb1b1d858cb55a5b336286366ceb92fe3c3dbdaa
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-05 19:08:58 +0300

    Added review item file

---
+++ src/views/components/review-item/ReviewItem.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {IReview} from "../../../types/IReview";
+import cn from 'classnames';
+import styles from './ReviewItem.module.scss'
+import {Button} from "@/shared-components/button/Button";
+import {EnumInfo} from "@/shared-components/enum-info/EnumInfo";
+import {BiLike} from "react-icons/bi";
+import {BiDislike} from "react-icons/bi";
+
+interface ReviewItemProps {
+    item: IReview;
+}
+
+export const ReviewItem: React.FC<ReviewItemProps> = ({item}) => {
+    const [isTruncated, setIsTruncated] = useState<boolean>(true);
+
+    const data = new Date(item.date)
+
+    return (
+        <div className={styles.item}>
+            <div className={styles.content}>
+                {item.title && <h3 className={styles.title}>{item.title}</h3>}
+                <p className={cn(styles.review, isTruncated && styles.truncated)}>{item.description}</p>
+                <div className={styles.bottom}>
+                    <Button style={styles.button} onClick={() => setIsTruncated(!isTruncated)}>
+                        {isTruncated ? 'Подробнее' : 'Скрыть'}
+                    </Button>
+
+                    <div className={styles.buttons}>
+                        <div className={styles.likes}>
+                            <Button style={styles.bi}>
+                                <BiLike/>
+                            </Button>
+                            <span>{item.positiveRating}</span>
+                        </div>
+
+                        <div className={styles.likes}>
+                            <Button style={styles.bi}>
+                                <BiDislike/>
+                            </Button>
+                            <span>{item.negativeRating}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <EnumInfo>
+                <li className={styles.author}>{item.author}</li>
+                <li className={styles.date}>{data.toLocaleDateString()}</li>
+            </EnumInfo>
+        </div>
+    );
+};
+
