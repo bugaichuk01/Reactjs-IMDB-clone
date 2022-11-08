c673d7b02073d5e4bb8db1ca56cf74757a0e7759
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-08 14:42:02 +0200

    Change logic with Content

--- src/views/components/home/films-group/FilmsGroup.tsx
+++ src/views/components/home/films-group/FilmsGroup.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styles from './FilmsGroup.module.scss';
 import {IMovie} from "../../../../types/IMovie";
+import {Content} from "@/layout-components/content/Content";
+import {IBaseQuery} from "../../../../types/IQuery";
 
 interface FilmsGroupTypes {
     title: string;
-    data: IMovie[] | undefined;
+    data: IBaseQuery<IMovie> | undefined;
 }
 
 export const FilmsGroup: React.FC<FilmsGroupTypes> = ({data, title}) => {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.films}>
-                {data?.slice(0, 3).map((item, index) => (
-                    <img key={index} src={item.posterUrl} alt={item.nameRu}/>
-                ))}
+        <Content data={data}>
+            <div className={styles.wrapper}>
+                <div className={styles.films}>
+                    {data?.[data.items ? 'items' : 'films']?.slice(0, 3).map((item, index) => (
+                        <img key={index} src={item.posterUrl} alt={item.nameRu}/>
+                    ))}
+                </div>
+                <span className={styles.description}>{title}</span>
             </div>
-            <span className={styles.description}>{title}</span>
-        </div>
+        </Content>
     );
 }
