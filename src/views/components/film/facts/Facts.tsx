8e3c224fde289202c7d0e2a3d744f9aaee432e7d
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-08 14:41:20 +0200

    Change logic with Content

--- src/views/components/film/facts/Facts.tsx
+++ src/views/components/film/facts/Facts.tsx
@@ -2,19 +2,20 @@ import styles from './Facts.module.scss';
 import React from "react";
 import {useGetFactsQuery} from "_/serviceAPI";
 import {IFact} from "../../../../types/IFact";
+import {Content} from "@/layout-components/content/Content";
 
 interface FilmFactsProps {
     filmId: number | undefined;
 }
 
 export const Facts: React.FC<FilmFactsProps> = (({filmId}) => {
-    const {data} = useGetFactsQuery(filmId)
+    const {data, isLoading, isFetching} = useGetFactsQuery(filmId)
 
     return (
-        <>
+        <Content data={data} isLoading={isLoading} isFetching={isFetching}>
             <ul className={styles.facts}>
-                {data?.items.map((el: IFact) => <li key={el.text} className={styles.item} dangerouslySetInnerHTML={{__html: el.text}} />)}
+                {data?.items?.map((el: IFact) => <li key={el.text} className={styles.item} dangerouslySetInnerHTML={{__html: el.text}} />)}
             </ul>
-        </>
+        </Content>
     )
 })
