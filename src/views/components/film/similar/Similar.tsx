b9948cdabe3eab77f932c25044fe5748143ee0d3
bugaichuk01 <bugaichuk001@gmail.com>
2022-11-08 14:42:53 +0200

    Change logic with Content

--- src/views/components/film/similar/Similar.tsx
+++ src/views/components/film/similar/Similar.tsx
@@ -3,31 +3,28 @@ import {useGetSimilarQuery} from "_/serviceAPI";
 import {SimilarItem} from "../similar-item/SimilarItem";
 import {Slider} from "@/shared-components/slider/Slider";
 import {Caption} from "@/shared-components/caption/Caption";
-import {Content} from "@/layout-components/content/Content";
 
 interface SimilarProps {
     id: number | undefined
 }
 
 export const Similar: React.FC<SimilarProps> = ({id}) => {
-    const {data, isLoading, isFetching} = useGetSimilarQuery(id)
+    const {data} = useGetSimilarQuery(id)
 
     return (
         <>
-            {data && data.total < 4
-                ? <Caption description={`К сожалению, мы не смогли найти похожих фильмов :(`}/>
+            {data && data.total < 6
+                ? <Caption description={`К сожалению, мы не смогли найти достаточное количетсво похожих фильмов :(`}/>
                 : (
                     <>
                         <Caption description={`Вам также может понравиться`} title='Похожее кино'/>
 
-                        <Content data={data} isLoading={isLoading} isFetching={isFetching}>
-                            <Slider
-                                itemsNumber={6}
-                                data={data?.items?.map((item) => (
-                                    <SimilarItem key={item.filmId} item={item}/>
-                                ))}
-                            />
-                        </Content>
+                        <Slider
+                            itemsNumber={6}
+                            data={data?.items?.map((item) => (
+                                <SimilarItem key={item.filmId} item={item}/>
+                            ))}
+                        />
                     </>
                 )
             }
